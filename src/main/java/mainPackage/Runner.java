079714b4ba78dc34ba843061dbd19e6814afc084
Your Name <skeletoro@github.com>
2021-05-26 06:35:40 -0700

    zh adding try catch for num mis match exception

--- src/main/java/mainPackage/Runner.java
+++ src/main/java/mainPackage/Runner.java
@@ -13,36 +13,42 @@ public class Runner {
 		do {
 			System.out.println("main menu \nselect an option \n1 for addition \n2 for subtraction "
 					+ "\n3 for multiplication \n4 for division \n0 to exit ");
-			selection = Integer.parseInt(scanner.nextLine());
+			try {
+				selection = Integer.parseInt(scanner.nextLine());
 
-			System.out.println("");
-			switch (selection) {
-			case 1:
-				Calculator.add();
-				break;
-			case 2:
-				Calculator.sub();
-				break;
+				System.out.println("");
+				switch (selection) {
+				case 1:
+					Calculator.add();
+					break;
+				case 2:
+					Calculator.sub();
+					break;
 
-			case 3:
-				Calculator.mult();
-				break;
+				case 3:
+					Calculator.mult();
+					break;
 
-			case 4:
-				Calculator.div();
-				break;
+				case 4:
+					Calculator.div();
+					break;
 
-			case 0:
-				System.out.println("Exiting. GOODBYE!");
-				cont = false;
-				break;
+				case 0:
+					System.out.println("Exiting. GOODBYE!");
+					cont = false;
+					break;
 
-			default:
-				System.out.println("invalid selection");
-				break;
+				default:
+					System.out.println("invalid selection");
+					break;
+				}
+
+			} catch (NumberFormatException e) {
+				System.out.println(e);
+
 			}
 		} while (cont);
 		scanner.close();
+		System.out.println("scanner closed");
 	}
-
 }
